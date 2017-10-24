c1b54f02b3dca5fa31ff5ed7f450e1ba2f4c3e02
Matias Micheletto <matiasmicheletto@gmail.com>
2017-10-24 09:43:56 -0300

    Implementado medicion continua de distancia

--- TestLaser/TestLaser.ino
+++ TestLaser/TestLaser.ino
@@ -1,15 +1,23 @@
+#include <Wire.h>
+#include <VL53L0X.h>
 
-
-
-
-
+VL53L0X sensor;
 
 void setup()
 {
+  Serial.begin(57600);
+  Wire.begin();
 
+  sensor.init();
+  sensor.setTimeout(300);
+  sensor.startContinuous();
+
+  Serial.println("Inicializacion terminada");
 }
 
 void loop()
 {
-
+  Serial.print(sensor.readRangeContinuousMillimeters());
+  if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
+  Serial.println();
 }
