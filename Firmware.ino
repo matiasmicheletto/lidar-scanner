f8725f54d3e43536873e036b92ec5c9c3666fafa
Matias <matiasmicheletto@gmail.com>
2017-12-18 15:24:03 -0300

    Cambios en el algoritmo de control de motores

--- Firmware.ino
+++ Firmware.ino
@@ -2,8 +2,9 @@
 #include "BYJ48.h"
 
 // Motores
-BYJ48 motor1(8,9,10,11);
-BYJ48 motor2(4,5,6,7);
+BYJ48 motor1(4,5,6,7);
+BYJ48 motor2(8,9,10,11);
+
 
 int posX = 0, posY = 0, spX = 0, spY = 0; // Posicion y setpoint
 boolean ready = true; // Estado
@@ -29,46 +30,63 @@ void setup(){
 ISR(TIMER2_COMPA_vect)
 // Interrupcion por timer 2 (10 ms)
 {
-  if(posX == spX && posY == spY){
+  // int dX = spX-posX;
+  // int dY = spY-posY;
+  short r1 = 0;
+  short r2 = 0;
+  int dM1 = spX-posX+spY-posY; // dX+dY
+  int dM2 = spX-posX-spY+posY; // dX-dY
+
+  if(dM1 > 0){
+    motor1.stepCW();
+    r1++;
+  }
+  else if(dM1 < 0){
+    motor1.stepCCW();
+    r1--;
+  }
+  else // dM1 == 0
     motor1.disable();
+
+  if(dM2 > 0){
+    motor2.stepCW();
+    r2++;
+  }
+  else if(dM2 < 0){
+    motor2.stepCCW();
+    r2--;
+  }
+  else // dM2 == 0;
     motor2.disable();
-    ready = true;
-    return;
-  }
-  if(posX < spX){ // Mover izquierda
-    motor1.stepCW();
-    motor2.stepCW();
-    posX++;
-    return;
-  }
-  if(posX > spX){ // Mover derecha
-    motor1.stepCCW();
-    motor2.stepCCW();
-    posX--;
-    return;
-  }
-  if(posY < spY){ // Mover arriba
-    motor1.stepCW();
-    motor2.stepCCW();
-    posY++;
-    return;
-  }
-  if(posY > spY){ // Mover abajo
-    motor1.stepCCW();
-    motor2.stepCW();
-    posY--;
-    return;
-  }
+    
+  posX += (r1+r2)/2;
+  posY += (r1-r2)/2;
+}
+
+void printAll(){
+  Serial.print("Pos = ("); Serial.print(posX);
+  Serial.print(","); Serial.print(posY); 
+  Serial.print(") -- SP = ("); Serial.print(spX);
+  Serial.print(","); Serial.print(spY);  
+  Serial.println(")");
+  delay(100);
 }
 
 void loop(){
   spX = 3000;
   spY = 0;
-  delay(5000);
+  while(spX != posX || spY != posY)
+    printAll();    
+  spX = 3000;
+  spY = 3000;
+  while(spX != posX || spY != posY)
+    printAll();
+  spX = 0;
+  spY = 3000;
+  while(spX != posX || spY != posY)
+    printAll();
   spX = 0;
   spY = 0;
-  delay(5000);
-  spX = 1500;
-  spY = 1500;
-  delay(5000);
+  while(spX != posX || spY != posY)
+    printAll();
 }
